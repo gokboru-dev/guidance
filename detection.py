1e569fa678c229b0c68c0c1e32d04f72040f9de0
ismailtrm <68031925+ismailtrm@users.noreply.github.com>
2022-07-04 20:47:19 +0300

    update

--- detection.py
+++ detection.py
@@ -8,7 +8,7 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture("1") #cap = cv2.VideoCapture("1")
+cap = cv2.VideoCapture(0) #cap = cv2.VideoCapture("1")
 
 upper_red = np.array([255, 255, 255])
 lower_red = np.array([171, 160, 60])
